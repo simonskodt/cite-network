a4477dde501deda63479d4f97fbc95ca8f7da9b7
Simon Johann Skødt <simon@skodt.dk>
2024-10-26 21:41:10 +0200

    chore: setup initial project skeleton

---
+++ src/main/java/com/simonskodt/citenetwork/CitenetworkApplication.java
@@ -0,0 +1,13 @@
+package com.simonskodt.citenetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitenetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitenetworkApplication.class, args);
+	}
+
+}
