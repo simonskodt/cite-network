e612da264e0348c09500dcb30cfeff73e5e8158f
Simon Johann Skødt <simon@skodt.dk>
2024-10-27 20:45:55 +0100

    chore: run an actual dummy test

--- src/test/java/com/simonskodt/citenetwork/CitenetworkApplicationTests.java
+++ src/test/java/com/simonskodt/citenetwork/CitenetworkApplicationTests.java
@@ -1,5 +1,11 @@
 package com.simonskodt.citenetwork;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CitenetworkApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void simpleTest() {
+		assertNotEquals(1, 2);
 	}
-
 }
