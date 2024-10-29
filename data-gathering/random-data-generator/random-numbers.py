9d38621e60b2e13df45e3d1d8e6f1c594597a8c1
Simon Johann Skødt <simon@skodt.dk>
2024-10-29 22:36:32 +0100

    feat: script to generate random relationships in graph databases

---
+++ data-gathering/random-data-generator/random-numbers.py
@@ -0,0 +1,32 @@
+import random
+
+# Create random relationships for the Neo4J database
+LOWER, UPPER = 1, 1000
+
+print("CSV filename")
+output_file = input()
+if not output_file.endswith(".csv"):
+    output_file = output_file + ".csv"
+
+print("Two relationship column must be provided")
+print("Column 1 name:")
+col1 = input()
+
+print("Column 2 name:")
+col2 = input()
+
+print("# of relationships to be made:")
+N = int(input())
+assert isinstance(N, int) and N > 0, "Number of relationships must be a positive integer"
+
+with open(output_file, "w") as file:
+    file.write(f"{col1},{col2}\n")
+    for i in range(1, N+1):
+        r1 = random.randint(LOWER, UPPER)
+        r2 = random.randint(LOWER, UPPER)
+        while r1 == r2: # assumption that papers does not cite itself
+            r1 = random.randint(LOWER, UPPER)
+        assert r1 != r2
+        file.write(f"{r1},{r2}\n")
+
+print(f"File {output_file} created")
