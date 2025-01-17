d87685f614792add832db3cc5d51b2ccc0601f7c
Simon Johann Skødt <simon@skodt.dk>
2025-01-17 21:27:16 +0100

    bug: update Paper entity to use descriptive property names and specify relationship directions

--- src/main/java/com/simonskodt/citenetwork/entities/Paper.java
+++ src/main/java/com/simonskodt/citenetwork/entities/Paper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
 import lombok.AllArgsConstructor;
@@ -18,14 +19,17 @@ import lombok.With;
 @Node
 public class Paper {
     @Id
-    private Long id;
+    private Long paperId;
+    @Property("title") // does not work
     private String title;
+    @Property("publicationYear") // same
     private int publicationYear;
+    @Property("doi") // same
     private String doi;
 
-    @Relationship(type = "CITES")
+    @Relationship(type = "CITES", direction = Relationship.Direction.OUTGOING)
     private List<Paper> cites;
 
-    @Relationship(type = "WRITTEN_BY")
+    @Relationship(type = "WRITTEN_BY", direction = Relationship.Direction.OUTGOING)
     private List<Author> authors;
 }
