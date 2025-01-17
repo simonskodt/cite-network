caeb7db5407a5843cd7b064093dfd6bb3cf6bbba
Simon Johann Skødt <simon@skodt.dk>
2025-01-17 19:56:21 +0100

    refactor: clean up CitenetworkApplicationTests by removing unused imports

--- src/test/java/com/simonskodt/citenetwork/CitenetworkApplicationTests.java
+++ src/test/java/com/simonskodt/citenetwork/CitenetworkApplicationTests.java
@@ -1,10 +1,6 @@
 package com.simonskodt.citenetwork;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
